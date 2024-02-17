b39b516de5cbe819b4f2281988a8ff13a32b7474
razikof <160317862+razikof@users.noreply.github.com>
2024-02-17 23:06:24 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	maxZoom: 20
+}).addTo(myMap);
